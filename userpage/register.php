389e629c6a4c365a56d6934058d16b97e4436d2f
lask20 <s6070040@kmitl.ac.th>
2015-12-13 08:51:18 +0700

    fix use geopoint

--- userpage/register.php
+++ userpage/register.php
@@ -2,138 +2,139 @@
 include "include/parse.php";
 use Parse\ParseUser;
 use Parse\ParseException;
+use Parse\ParseGeoPoint;
 
 if (!empty($_POST['email'])) {
-    $user = new ParseUser();
+  $user = new ParseUser();
 //$user->set("username", $_POST['email']);
-$user->set("password", $_POST['password']);
-$user->set("email", $_POST['email']);
-$user->set("phone", $_POST['phone']);
-$user->set("fullName", $_POST['fullname']);
+  $user->set("password", $_POST['password']);
+  $user->set("email", $_POST['email']);
+  $user->set("phone", $_POST['phone']);
+  $user->set("fullName", $_POST['fullname']);
 
-$ne = new ParseGeoPoint($_POST['NorthEastLat'], $_POST['NorthEastLng']);
-$sw = new ParseGeoPoint($_POST['SouthWestLat'], $_POST['SouthWestLng']);
-$location = [$ne, $sw];
-$user->setArray("location", $location);
+  $ne = new ParseGeoPoint($_POST['NorthEastLat'], $_POST['NorthEastLng']);
+  $sw = new ParseGeoPoint($_POST['SouthWestLat'], $_POST['SouthWestLng']);
+  $location = [$ne, $sw];
+  $user->setArray("location", $location);
 
 
-try {
-  $user->signUp();
+  try {
+    $user->signUp();
   // Hooray! Let them use the app now.
-} catch (ParseException $ex) {
+  } catch (ParseException $ex) {
   // Show the error message somewhere and let the user try again.
-  echo "Error: " . $ex->getCode() . " " . $ex->getMessage();
-}
+    echo "Error: " . $ex->getCode() . " " . $ex->getMessage();
   }
+}
 
 
 ?>
 
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>A.S.H.I | Registration Page</title>
-    <!-- Tell the browser to be responsive to screen width -->
-      <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-    
-    <!-- Bootstrap 3.3.5 -->
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
-    <!-- iCheck -->
-    <link rel="stylesheet" href="plugins/iCheck/square/blue.css">
-    <style>
-      html, body {
-        height: 50%;
-        margin: 0;
-        padding: 0;
-        background-color: #E6E6E6;
-      }
-      #map {
-        min-height: 400px;
-        min-width: 20%;
-        border:2px solid white;
-        margin-left: 30%;
-        margin-right: 30%;
-        margin-top: 1%;
-        margin-bottom: 2%;
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>A.S.H.I | Registration Page</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
 
-      }
-      .wrappp{
+  <!-- Bootstrap 3.3.5 -->
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="plugins/iCheck/square/blue.css">
+  <style>
+    html, body {
+      height: 50%;
+      margin: 0;
+      padding: 0;
+      background-color: #E6E6E6;
+    }
+    #map {
+      min-height: 400px;
+      min-width: 20%;
+      border:2px solid white;
+      margin-left: 30%;
+      margin-right: 30%;
+      margin-top: 1%;
+      margin-bottom: 2%;
 
-        margin-top: 3%;
-        margin-left: 30%;
-        margin-right: 30%;
-        height:auto;
-     
-      }
-      .wrapB{
-        width:400px;
-        height:auto;
-        margin:auto;
-        margin-bottom:10px;
-        text-align: center;
-        position:relative;
-      }
+    }
+    .wrappp{
 
-    </style>
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+      margin-top: 3%;
+      margin-left: 30%;
+      margin-right: 30%;
+      height:auto;
+
+    }
+    .wrapB{
+      width:400px;
+      height:auto;
+      margin:auto;
+      margin-bottom:10px;
+      text-align: center;
+      position:relative;
+    }
+
+  </style>
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
+        <![endif]-->
+      </head>
+      <body>
 
-  <div class="wrappp">
-  <div class="register-logo">
-        <b>A.S.H.I.</b>&nbsp;Register
-    </div>
-  <form action="#" method="post">
-          <label for="exampleInputFile">Profile Picture</label>
-          <input type="file" id="exampleInputFile"></br>
-          <input name="NorthEastLat" type="hidden" id="NorthEastLat">
-          <input name="NorthEastLng" type="hidden" id="NorthEastLng">
-          <input name="SouthWestLat" type="hidden" id="SouthWestLat">
-          <input name="SouthWestLng" type="hidden" id="SouthWestLng">
+        <div class="wrappp">
+          <div class="register-logo">
+            <b>A.S.H.I.</b>&nbsp;Register
+          </div>
+          <form action="#" method="post">
+            <label for="exampleInputFile">Profile Picture</label>
+            <input type="file" id="exampleInputFile"></br>
+            <input name="NorthEastLat" type="hidden" id="NorthEastLat">
+            <input name="NorthEastLng" type="hidden" id="NorthEastLng">
+            <input name="SouthWestLat" type="hidden" id="SouthWestLat">
+            <input name="SouthWestLng" type="hidden" id="SouthWestLng">
 
-          <div class="form-group has-feedback">
-            <input name="email" type="email" class="form-control" placeholder="Email">
-            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+            <div class="form-group has-feedback">
+              <input name="email" type="email" class="form-control" placeholder="Email">
+              <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+            </div>
+            <div class="form-group has-feedback">
+              <input name="password" type="password" class="form-control" placeholder="Password">
+              <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+            </div>
+            <div class="form-group has-feedback">
+              <input name="repassword" type="password" class="form-control" placeholder="Retype password">
+              <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
+            </div>
+            <div class="form-group has-feedback">
+              <input name="fullname" type="text" class="form-control" placeholder="Full name">
+              <span class="glyphicon glyphicon-user form-control-feedback"></span>
+            </div>
+            <div class="form-group has-feedback">
+              <input name="phone" type="text" class="form-control" placeholder="Phone number">
+              <span class="glyphicon glyphicon-phone-alt form-control-feedback"></span>
+            </div>
+            <div class="form-group has-feedback">
+              <textarea name="address" class="form-control" rows="3" placeholder="Address"></textarea>
+              <span class="glyphicon glyphicon-home form-control-feedback"></span>
+            </div>
+            <b>Location</b>&nbsp;&nbsp;( select area )
           </div>
-          <div class="form-group has-feedback">
-            <input name="password" type="password" class="form-control" placeholder="Password">
-            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-          </div>
-          <div class="form-group has-feedback">
-            <input name="repassword" type="password" class="form-control" placeholder="Retype password">
-            <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
-          </div>
-          <div class="form-group has-feedback">
-            <input name="fullname" type="text" class="form-control" placeholder="Full name">
-            <span class="glyphicon glyphicon-user form-control-feedback"></span>
-          </div>
-          <div class="form-group has-feedback">
-            <input name="phone" type="text" class="form-control" placeholder="Phone number">
-            <span class="glyphicon glyphicon-phone-alt form-control-feedback"></span>
-          </div>
-          <div class="form-group has-feedback">
-            <textarea name="address" class="form-control" rows="3" placeholder="Address"></textarea>
-            <span class="glyphicon glyphicon-home form-control-feedback"></span>
-          </div>
-           <b>Location</b>&nbsp;&nbsp;( select area )
-  </div>
 
-            <div id="map"></div>
+          <div id="map"></div>
 
-    <script>
+          <script>
 
 // This example adds a user-editable rectangle to the map.
 // When the user changes the bounds of the rectangle,
@@ -184,8 +185,8 @@ function showNewRect(event) {
   $("#SouthWestLng").val(sw.lng());
 
   var contentString = '<b>Rectangle moved.</b><br>' +
-      'New north-east corner: ' + ne.lat() + ', ' + ne.lng() + '<br>' +
-      'New south-west corner: ' + sw.lat() + ', ' + sw.lng();
+  'New north-east corner: ' + ne.lat() + ', ' + ne.lng() + '<br>' +
+  'New south-west corner: ' + sw.lat() + ', ' + sw.lng();
 
   // Set the info window's content and position.
   infoWindow.setContent(contentString);
@@ -194,23 +195,23 @@ function showNewRect(event) {
   infoWindow.open(map);
 }
 
-    </script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBeQIal1IZm15eZ4iDBkOKDqjNRTTNirxU&callback=initMap&signed_in=true" async defer>
-    </script>
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBeQIal1IZm15eZ4iDBkOKDqjNRTTNirxU&callback=initMap&signed_in=true" async defer>
+</script>
 
 
-              <div class="wrapB"><button class="btn btn-block btn-primary btn-lg">Register</button></div>
+<div class="wrapB"><button class="btn btn-block btn-primary btn-lg">Register</button></div>
 
 
 </form>
-       
 
-    <!-- jQuery 2.1.4 -->
-    <script src="plugins/jQuery/jQuery-2.1.4.min.js"></script>
-    <!-- Bootstrap 3.3.5 -->
-    <script src="bootstrap/js/bootstrap.min.js"></script>
-    <!-- iCheck -->
-    <script src="plugins/iCheck/icheck.min.js"></script>
 
-  </body>
+<!-- jQuery 2.1.4 -->
+<script src="plugins/jQuery/jQuery-2.1.4.min.js"></script>
+<!-- Bootstrap 3.3.5 -->
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<!-- iCheck -->
+<script src="plugins/iCheck/icheck.min.js"></script>
+
+</body>
 </html>
