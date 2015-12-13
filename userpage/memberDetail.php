966d0906a9b109f2b6330a91978b16a50c7ba221
xillios <xilios@hotmail.com>
2015-12-14 00:39:35 +0700

    add Admin image
    add image

--- userpage/memberDetail.php
+++ userpage/memberDetail.php
@@ -122,7 +122,7 @@ $location = $results->get('location');
           <!-- Sidebar user panel -->
           <div class="user-panel">
             <div class="pull-left image">
-              <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+              <img src="dist/img/Admin.png" class="img-circle" alt="User Image">
             </div>
             <div class="pull-left info">
               <p>Admin</p>
