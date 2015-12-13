48032cd017063d04c516429b60df27738625f1b7
xillios <xilios@hotmail.com>
2015-12-14 00:33:22 +0700

    add User image

--- userpage/sendmsg.php
+++ userpage/sendmsg.php
@@ -82,7 +82,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel">
           <div class="pull-left image">
-            <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+            <img src="dist/img/User.png" class="img-circle" alt="User Image">
           </div>
           <div class="pull-left info">
            <p><?php echo $currentUser->getUsername(); ?></p>
