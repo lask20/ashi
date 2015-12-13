8905c7c5cd42aed69513b2c00b391aa00e169476
xillios <xilios@hotmail.com>
2015-12-14 00:42:11 +0700

    add image
    add image

--- userpage/confirmRegis.php
+++ userpage/confirmRegis.php
@@ -103,7 +103,7 @@ $results = $query->find();
               <!-- Sidebar user panel -->
               <div class="user-panel">
                 <div class="pull-left image">
-                  <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                  <img src="dist/img/Admin.png" class="img-circle" alt="User Image">
                 </div>
                 <div class="pull-left info">
                   <p><?php echo $currentUser->getUsername(); ?></p>
