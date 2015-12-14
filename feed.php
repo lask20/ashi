b1c4f94176b2953bb38e3b597e5e5d64d5f2b23a
lask20 <s6070040@kmitl.ac.th>
2015-12-14 13:33:28 +0700

    cv

--- feed.php
+++ feed.php
@@ -9,5 +9,5 @@ $query = new ParseQuery("NotiData");
 $query->descending("createdAt");
 $object = $query->first();
 
-echo $object->get("fullname")." : ".$object->get("message");
+echo $object->get("fullName")." : ".$object->get("message");
 ?>
